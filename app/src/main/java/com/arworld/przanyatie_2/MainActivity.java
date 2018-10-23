439fb5213f95fc2684fb37686fdb9e45e6f7297f
Smaylz <savich.stanislav@mail.ru>
2018-10-24 02:43:48 +0300

    start

---
+++ app/src/main/java/com/arworld/przanyatie_2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.arworld.przanyatie_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView age = (TextView)findViewById(R.id.age);
+
+        SeekBar seekBar = findViewById(R.id.age_progress);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                age.setText(progress + " лет");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
